6e6aa79e19defa088586b227b4bf6a06bfc775f4
Paul Vollmer <wrongentertainment@yahoo.de>
2011-12-03 09:56:17 +0100

    php test failed

--- nav.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		<a href="index.php">Introduction</a> | 
-		<a href="guide.php">User Guide</a> | 
-		<a href="download.php">Download</a> | 
-		<a href="contact.php">Contact</a> <br/>
-	</body>	
-</html>
