24ad2762f3dbbd2282dc8021281d03c814a1af0a
Paul Vollmer <wrongentertainment@yahoo.de>
2011-12-15 15:49:06 +0100

    Added textConsole class

---
+++ src/TextConsole.h
@@ -0,0 +1,58 @@
+/**
+ * MagicTrackpadOsc is developed by Paul Vollmer (wrong-entertainment.com)
+ * 
+ * 
+ * Copyright (c) 2011 Paul Vollmer
+ *
+ * MagicTrackpadOsc is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * MagicTrackpadOsc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with MagicTrackpadOsc; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Paul Vollmer
+ * @modified    2011.12.15
+ * @version     0.1.1
+ */
+
+
+
+#pragma once
+
+#include "ofMain.h"
+
+#define NUM_STRINGS 20
+
+
+
+
+class TextConsole {
+	
+	public:
+		/* Constructor */
+		TextConsole();
+	
+	
+		/* Methods */
+		void init();
+		void display(int _x, int _y);
+		void addString(string msg);
+	
+	
+		/* Variables */
+		int    currentConsoleStrings;
+		string consoleStrings[NUM_STRINGS];
+	
+	
+	private:
+	
+};
